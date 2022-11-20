cae5ab93d6499d36e0d3bd88f6acdf7bf4c47655
opheliah95 <opheliah95@gmail.com>
2022-11-20 20:17:12 +0000

    feat: added initial typescript code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
-
-import confetti from 'canvas-confetti';
-
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const form = document.querySelector<HTMLFormElement>("#new-task-form") 
+const list = document.querySelector("#list") as HTMLUListElement
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+console.log(input?.value)
