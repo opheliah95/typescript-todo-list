11b3729423c9d4609f7b4463dd1d6ef6434f99e1
opheliah95 <opheliah95@gmail.com>
2022-11-21 00:26:15 +0000

    feat: save and load task

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,42 @@ type Task = {
   createdAt: Date;
 };
 
+// load previous tasks
+const loadTasks = (): Task[] => {
+  const tasks_all = localStorage.getItem('TASKS');
+  const tasks_arr: Task[] = tasks_all !== null ? JSON.parse(tasks_all) : [];
+  return tasks_arr;
+};
+
+// function to create new task
+const createNewTask = (task: Task) => {
+  const item = document.createElement('li');
+  // html label
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+
+  // update check
+  checkbox?.addEventListener('change', (e) => {
+    task.completed = checkbox.checked;
+    console.log(`${task.title} has been completed ${task.completed}`);
+    saveTasks(tasks);
+  });
+  // add extra item
+  label.append(task.title, checkbox);
+  item.append(label);
+  list?.append(item);
+
+  // add task to array
+  tasks.push(task);
+};
+
+// function to save and load task locally
+const tasks: Task[] = loadTasks();
+console.log(`tasks are ${tasks}`);
+tasks.forEach(createNewTask);
+
 // listens to input event
 form?.addEventListener('submit', (e) => {
   e.preventDefault(); // prevent how by default click is handled
@@ -31,17 +67,14 @@ form?.addEventListener('submit', (e) => {
   };
 
   console.log(task);
+  // create and save the task
   createNewTask(task);
+  saveTasks(tasks);
+  // set input to empty
+  input.value = '';
 });
 
-const createNewTask = (task: Task) => {
-  const item = document.createElement('li');
-  // html label
-  const label = document.createElement('label');
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  // add extra item
-  label.append(task.title, checkbox);
-  item.append(label);
-  list?.append(item);
+// function to save task locally
+const saveTasks = (tasks: Task[]) => {
+  localStorage.setItem('TASKS', JSON.stringify(tasks));
 };
